beb3c0a40add170bdfffbc15fec5bcd4549d7bc1
Andrew M. Silva <andrewsaxx@gmail.com>
2024-02-25 09:14:02 -0300

    feat: using pt-br as default

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import { languages } from "./module/languages.js";
 import { translateJsons } from "./module/translate-jsons.js";
 
 (async () => {
-  await translateJsons();
+  await translateJsons(languages.Portuguese_Brazil);
 })();
